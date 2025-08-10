858e8e5f0bec223489431e71f410e8e6a2c76bb8
Youssef-Malware <youssef.mansour3129@gmail.com>
2025-08-10 03:24:34 -0700

    print("helloultra69")

---
+++ commite-change.py
@@ -0,0 +1,4 @@
+print("hello69")
+#To make repo i went in get hub and wrote its name then turned on README. then i wrote in the discription then i went in VScode and cloned it from ther
+#To change commite i wrote down a code then i wrote the code in the commite section then i clicked change commite
+print("helloultra69")
