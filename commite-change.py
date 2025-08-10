ef9466909466ea3cd593edfa9565ea4e4370129b
Youssef-Malware <youssef.mansour3129@gmail.com>
2025-08-10 03:57:44 -0700

    print("helloomega69")

--- commite-change.py
+++ commite-change.py
@@ -2,3 +2,5 @@ print("hello69")
 #To make repo i went in get hub and wrote its name then turned on README. then i wrote in the discription then i went in VScode and cloned it from ther
 #To change commite i wrote down a code then i wrote the code in the commite section then i clicked change commite
 print("helloultra69")
+#I went in GITLENS and right-clicked on the main branch it opened up a menu and i pressed create new branch then too delete i went to the graph and right-clicked on the main branch and pressed delete branch
+print("helloomega69")
